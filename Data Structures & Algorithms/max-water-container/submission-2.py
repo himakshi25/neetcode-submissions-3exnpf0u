48bc5364075665a5322e63c3a9442902c1a6e04b
Himakshi Salhotra <salhotrahimakshi25@gmail.com>
2026-08-05 18:31:30 -0400

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.py
@@ -0,0 +1,14 @@
+# use 2 pointer then greedy approach to keep big element and move pointer where element is smalled
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        l=0
+        r=len(heights)-1
+        ans=0
+        while l<r:
+            ans=max(ans, (r-l)*(min(heights[l], heights[r])))
+            if heights[l]<heights[r]:
+                l+=1
+            else:
+                r-=1
+        return ans
+        
