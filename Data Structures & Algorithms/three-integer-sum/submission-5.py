35e7fa05b2406c77c9a5c1a525ba2211fcf58a16
Himakshi Salhotra <salhotrahimakshi25@gmail.com>
2026-08-05 13:50:13 -0400

    Add: three-integer-sum - submission-5

---
+++ Data Structures & Algorithms/three-integer-sum/submission-5.py
@@ -0,0 +1,24 @@
+# this can be reduced to 2 sum and finding target. fix one element and do 2 pointer on remaining sorted array.
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        ans = set()
+        for i, num in enumerate(nums):
+            l=i+1
+            r=len(nums)-1
+            while l<r:
+                val = num + nums[l] + nums[r]
+                if val== 0:
+                    ans.add((num,nums[l],nums[r]))
+                    r-=1
+                    l+=1
+                elif val<0:
+                    l+=1
+                else:
+                    r-=1
+        return list(ans)
+
+
+
+        
